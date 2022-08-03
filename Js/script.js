4d369fdb4faa3e7018d7b488b8c7bb79cc411769
Masum Alam <masum.7788alam@gmail.com>
2022-08-03 20:28:36 +0530

    changes

--- Js/script.js
+++ Js/script.js
@@ -22,7 +22,24 @@ $(document).ready(function () {
     $('.hero-slide').slick({
         prevArrow:'<button class="slide-left" type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
         nextArrow: '<button class="slide-right" type="button" class= "slick-prev" > <i class="fas fa-angle-left"></i></button> ',
-    
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    arrows: false,
+                    speed: 300,
+                    fade: true,
+                    cssEase: 'linear',
+                    autoPlay: true,
+                    autoplaySpeed: 300,
+                    dots: true,
+                }
+            
+            
+            }
+        
+        
+        ]
     })
 
     /*=========nav-scrolling============*/
@@ -33,11 +50,13 @@ $(document).ready(function () {
             $('#navScroll').addClass('nav-scroll');
             $('#brand').addClass('fill');
             $('a').addClass('black');
+            $('.ham-menu').addClass('bg-color');
         }
         else {
             $('#navScroll').removeClass('nav-scroll');
             $('#brand').removeClass('fill');
             $('a').removeClass('black');
+            $('.ham-menu').removeClass('bg-color');
             
             
         }
@@ -56,6 +75,21 @@ $(document).ready(function () {
         slidesToShow: 2,   
         slidesToScroll: 2,
         arrows: false,
+        autoPlay: true,
+        autoplaySpeed: 500,
+        
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToScroll: 1,
+                    slidesToShow: 1,
+                }
+        
+            
+            },
+        ]
+      
 
     }) 
     /*===============partner-slide========*/
@@ -63,8 +97,29 @@ $(document).ready(function () {
         slidesToScroll: 5,
         slidesToShow: 5,
         arrows:false,
+        autoPlay: true,
+        autoplaySpeed: 500, 
+        infinite: true,
 
-    
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToScroll: 3,
+                    slidesToShow: 3,
+                    
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToScroll: 2,
+                    slidesToShow: 2,
+                }
+            
+            }
+        ]
+
     })
     
 })
